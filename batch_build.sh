1adb6683618abe4bb4f84a4382248a8f3aea3d53
abiakarn <nora.abiakar@gmail.com>
2019-02-19 17:23:26 +0100

    wip

--- batch_build.sh
+++ batch_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -l
-#SBATCH --job-name=mc_test
+#SBATCH --job-name=build_test
 #SBATCH --nodes=1
 #SBATCH --constraint=mc
 #SBATCH --time=00:15:00
+#SBATCH --output=build.out
 
 ./install-local.sh arbor -e ./systems/daint-mc.sh
 
--- scripts/batch_run.sh
+++ scripts/batch_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
-#SBATCH --job-name=mc_test
+#SBATCH --job-name=run_test
 #SBATCH --nodes=1
 #SBATCH --constraint=mc
 #SBATCH --time=00:15:00
+#SBATCH --output=run.out
 
 ./run-bench.sh arbor --model "ring" --config "small"
